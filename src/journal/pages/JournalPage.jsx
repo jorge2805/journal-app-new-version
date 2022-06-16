ebf7d7b92da78583025c3e06c56421406e721ad0
Jorge Cuevas <20152704@itla.edu.do>
2022-06-15 23:32:50 -0400

    Creating JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { Phone } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Irure aliqua est quis ullamco consequat.Eu ullamco dolor consequat tempor exercitation adipisicing. Pariatur deserunt aliqua ea dolore esse excepteur sunt id pariatur pariatur pariatur quis voluptate ullamco. Duis labore excepteur minim do.</Typography>
+    </JournalLayout>
   )
 }
