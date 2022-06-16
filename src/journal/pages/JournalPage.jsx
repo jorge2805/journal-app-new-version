c3db8b31e977b1aa9296efdee83b43238109c460
Jorge Cuevas <20152704@itla.edu.do>
2022-06-16 18:09:46 -0400

    Adding Note View

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>      
-      <NothingSelectedView/>
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
     </JournalLayout>
   )
 }
--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,4 +1,4 @@
-import { StarOutline } from "@mui/icons-material"
+import { BookmarkAdd } from "@mui/icons-material"
 import { Grid, Typography } from "@mui/material"
 
 export const NothingSelectedView = () => {
@@ -12,7 +12,7 @@ export const NothingSelectedView = () => {
         sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 3}}
     >
         <Grid item xs={12}>
-            <StarOutline sx={{ fontSize: 100, color: 'white'}}/>
+            <BookmarkAdd sx={{ fontSize: 100, color: 'white'}}/>
         </Grid>
         <Grid item xs={12}>
             <Typography color="white" variant="h5">Selecciona o Crea una entrada</Typography>
