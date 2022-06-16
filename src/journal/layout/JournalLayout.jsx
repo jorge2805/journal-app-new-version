7450ff96b889275db3f2eb74f2b397e1dd2e0ee8
Jorge Cuevas <20152704@itla.edu.do>
2022-06-16 09:28:33 -0400

    Creating NavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,13 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components"
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{ display: 'flex' }}>
 
-        {/* NavBar */}
+        <NavBar drawerWidth={drawerWidth}/>
 
         {/* Sidebar */}
 
