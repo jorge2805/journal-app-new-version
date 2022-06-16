440e37823554ef7b93c17b65b5e551825037d776
Jorge Cuevas <20152704@itla.edu.do>
2022-06-16 09:55:20 -0400

    Adding SideBar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,46 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+
+export const SideBar = ({drawerWidth = 240}) => {
+  return (
+    <Box
+        component='nav'
+        sx={{width: { sm: drawerWidth}, flexShrink: { sm: 0}}}
+    >
+        <Drawer
+            variant="permanent"
+            open
+            sx={{
+                display: { xs: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth}
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" noWrap component="div">
+                    Jorge
+                </Typography>
+            </Toolbar>
+            <Divider/>
+
+            <List>
+                {
+                    ['January','February','March','April'].map( text => (
+                        <ListItem key={ text } disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot/>
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text}/>
+                                    <ListItemText secondary={ 'Minim labore magna enim et dolor culpa veniam.'} />
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+        </Drawer>
+
+    </Box>
+  )
+}
