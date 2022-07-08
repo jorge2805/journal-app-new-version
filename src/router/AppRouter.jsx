2c3b04d34e8a019e610ba729947717fd20a7deee
Jorge Cuevas <20152704@itla.edu.do>
2022-07-08 18:42:32 -0400

    Adding Login Behaviour

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,31 @@
-import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from "react-router-dom"
+
+import { onAuthStateChanged } from "firebase/auth"
+import { FirebaseAuth } from "../firebase/config"
 
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import CheckingAuth from "../ui/components/checkingAuth"
+import { login, logout } from "../store/auth"
+import { CheckingAuth } from "../ui"
+
 
 export const AppRouter = () => {
   
   const {status} = useSelector( state => state.auth)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged( FirebaseAuth, async(user) => {
+      if(!user) return dispatch(logout());
+
+      const {uid, email, displayName, photoURL} = user;
+      dispatch(login( {uid, email, displayName, photoURL}) );
+    })
+  }, [])
+  
 
   if(status === 'checking') return <CheckingAuth/>
   
