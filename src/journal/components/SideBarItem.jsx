4f4c25d2700d3dbbffb5feff7f15439eec6b3d97
Jorge Cuevas <20152704@itla.edu.do>
2022-07-13 19:14:43 -0400

    updating active note with OnClick

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,9 +1,13 @@
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { TurnedInNot } from '@mui/icons-material'
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({title = '', body = ''}) => {
+export const SideBarItem = ({id, title = '', body = '', date, imageUrls}) => {
   
+  const dispatch = useDispatch();
+
   const trunkTitle = useMemo(() => {
     return title.length > 17 
       ? title.substring(0,17) + '...'
@@ -16,9 +20,15 @@ export const SideBarItem = ({title = '', body = ''}) => {
       : body;
   }, [body])
   
+  const handleListItem = () => {
+    dispatch(setActiveNote({id, title, body, date, imageUrls}));
+  }
+
   return (
     <ListItem disablePadding>
-        <ListItemButton>
+        <ListItemButton
+          onClick={ handleListItem }
+        >
             <ListItemIcon>
                 <TurnedInNot/>
             </ListItemIcon>
