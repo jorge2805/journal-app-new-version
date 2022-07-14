c6530014b82ae25f539d27809798a5295a1e9dbb
Jorge Cuevas <20152704@itla.edu.do>
2022-07-14 18:31:39 -0400

    update note view

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,46 +1,65 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
-import { ImageGallery } from "../components"
+import { SaveOutlined } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks";
+import { ImageGallery } from "../components";
+
 
 export const NoteView = () => {
-  return (
-    <Grid 
-        container
-        direction='row'
-        justifyContent='space-between'
-        alignItems='center'
-        sx={{mb: 1}}
-    >
-        <Grid item>
-            <Typography fontSize={39} fontWeight='light'>16 de Junio 2022</Typography>
-        </Grid>
-        <Grid item> 
-            <Button color="primary" sx={{padding: 2}} >
-                <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
-                Save
-            </Button>
-        </Grid>
-        <Grid container>
-            <TextField
-                type="text"
-                variant="filled"
-                fullWidth
-                placeholder="Insert Tittle"
-                label="Title"
-                sx={{ border: 'none', mb: 1}}
-            />
-            <TextField
-                type="text"
-                variant="filled"
-                fullWidth
-                multiline
-                placeholder="Tell me what happened today"
-                minRows={5}
-                sx={{ border: 'none', mb: 1}}
-            />
-        </Grid>
+    
+    const {active: activeNote} = useSelector(state => state.journal)
+  
+    const {id, title, body, date, onInputChange, formState} = useForm(activeNote);
 
-        <ImageGallery/>
-    </Grid>
-  )
+    const dateString = useMemo(() => {
+        return new Date(date).toUTCString();
+    }, [date])
+
+    return (
+        <Grid 
+            container
+            direction='row'
+            justifyContent='space-between'
+            alignItems='center'
+            sx={{mb: 1}}
+        >
+            <Grid item>
+                <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
+            </Grid>
+            <Grid item> 
+                <Button color="primary" sx={{padding: 2}} >
+                    <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
+                    Save
+                </Button>
+            </Grid>
+            <Grid container>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder="Insert Tittle"
+                    label="Title"
+                    sx={{ border: 'none', mb: 1}}
+                    name="title"
+                    value={ title }
+                    onChange={onInputChange}
+                />
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="Tell me what happened today"
+                    minRows={5}
+                    sx={{ border: 'none', mb: 1}}
+                    name="body"
+                    value={ body  }
+                    onChange={onInputChange}
+                />
+            </Grid>
+
+            <ImageGallery/>
+        </Grid>
+    )
 }
